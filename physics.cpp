0c0493dae8a681ecb5649a90410713a143c06798
Alpagateau <nadaud1404@gmail.com>
2025-02-28 20:04:31 +0100

    Working on the physics

--- physics.cpp
+++ physics.cpp
@@ -17,33 +17,61 @@ void Rigidbody::AddForce(Vector2 force)
   force_acc = Vector2Add(force, force_acc);
 }
 
-bool Rigidbody::CheckCollisionStatic(Rectangle other)
+Rectangle Rigidbody::GetWorldCollision()
 {
-  Rectangle world_space = (Rectangle)
+  Rectangle world_space_a = (Rectangle)
   {
       position.x + collision.x,
       position.y + collision.y,
       collision.width,
       collision.height
   };
-  return CheckCollisionRecs(world_space, other);
+
+  return world_space_a;
 }
 
-bool Rigidbody::CheckCollisionTilemap(tilemap& tm)
+PhysicWorld::PhysicWorld()
+{
+  bodies.clear();
+  gravity = {0, 0};
+}
+
+bool PhysicWorld::CheckCollision(Rigidbody* a, Rigidbody* b)
+{
+  Rectangle world_space_a = (Rectangle)
+  {
+      a->position.x + a->collision.x,
+      a->bposition.y + a->collision.y,
+      a->collision.width,
+      a->collision.height
+  };
+
+  Rectangle world_space_b = (Rectangle)
+  {
+      b->position.x + b->collision.x,
+      b->position.y + b->collision.y,
+      b->collision.width,
+      b->collision.height
+  };
+
+  return CheckCollisionRecs(world_space_a, world_space_b);
+}
+/*
+bool PhysicWorld::CheckCollisionTilemap(Rigidbody a, tilemap& tm)
 {
   bool isGood = true;
   Rectangle world_space = (Rectangle)
   {
-      position.x + collision.x,
-      position.y + collision.y,
-      collision.width,
-      collision.height
+      a->osition.x + a->collision.x,
+      a->position.y + a->collision.y,
+      a->collision.width,
+      a->collision.height
   };
   for(int i = 0; i < tm.tiles.size(); i++)
   {
     if(isGood)
     {
-      if(collision_mask[tm.tiles[i].val])
+      if(a->collision_mask[tm.tiles[i].val])
       {
         Rectangle r = (Rectangle)
         {
@@ -61,24 +89,51 @@ bool Rigidbody::CheckCollisionTilemap(tilemap& tm)
   }
   return !isGood;
 }
+*/
 
-void Rigidbody::Update(float dt, tilemap& tm)
+void PhysicsWorldUpdate(float dt)
 {
-  force_acc = Vector2Scale(force_acc, dt/mass);
-  velocity = Vector2Add(velocity, force_acc);
-  force_acc = (Vector2){0, 0};
-  Vector2 scaled_speed = Vector2Scale(velocity, dt);
-  position.x += scaled_speed.x;
-  if(CheckCollisionTilemap(tm))
-  {
-    position.x -= scaled_speed.x;
-    velocity.x = 0;
-  }
-  position.y += scaled_speed.y;
-  if(CheckCollisionTilemap(tm))
-  {
-    position.y -= scaled_speed.y;
-    velocity.y = 0;
+  //First Apply Collisions
+  for(int i = 0; i < bodies.size(); i++)
+  { 
+    for(int j = i; j < bodies.size(); j++)
+    {
+      if(i!=j)
+      {
+        if(bodies[i]->static && bodies[j]->static)
+          continue;
+        if(CheckCollision(bodies[i], bodies[j]))
+        {
+          Rectangle col_rec = GetCollisionRec(bodies[i]->GetWorldCollision(), bodies[j]->GetWorldCollision());
+          float ratio = (bodies[i]->mass)/(bodies[i]->mass + bodies[j]->mass);
+          
+          //multipliers depending on the quadrant of the collision;
+          int x_mul = 1;
+          int y_mul = 1;
+          //Check quadrant
+          
+          Vector2 delta = 
+
+          //Apply collision
+          if(bodies[i]->static)
+          {
+            ratio =1;
+          }
+          if(bodies[j]->static)
+          {
+            ratio = 0;
+          } 
+          
+          bodies[i]->position.x -= (1-ratio)*x_mul*col_rec.width;
+          bodies[i]->position.y -= (1-ratio)*y_mul*col_rec.height;
+          
+          bodies[j]->position.x += ratio*x_mul*col_rec.width;
+          bodies[j]->position.y += ratio*y_mul*col_rec.height;
+        }
+      }
+    }
   }
 }
+
+
 
--- physics.hpp
+++ physics.hpp
@@ -3,12 +3,20 @@
 #include "raylib.h"
 #include "tiles.hpp"
 #include "raymath.h"
+#include <vector>
 
 //-------------------------------
 //         How It Works 
 //-------------------------------
-//-> Every physics object added to a pool 
+//-> PhysicWorld :
+//  -> A list of constants 
+//  -> A vector of Rigidbodies
 //
+//->  Rigidbody :
+//  -> static / dynamic 
+//  -> position 
+//  -> mass 
+//  -> collider (axis align box)
 
 class Rigidbody 
 {
@@ -18,13 +26,25 @@ public:
   Rectangle collision;
   float mass;
   bool collision_mask[64];
+  bool static = false;
   Vector2 force_acc;
+  int collision_layer;
 
   Rigidbody();
   void AddForce(Vector2 force);
-  void Update(float dt, tilemap& tm);
-  bool CheckCollisionStatic(Rectangle other);
-  bool CheckCollisionTilemap(tilemap& tm);
+  //void Update(float dt, tilemap& tm);
+  Rectangle GetWorldCollision();
+};
+
+class PhysicWorld 
+{
+public:
+  Vector2 gravity;
+  std::vector<Rigidbody*> bodies;
+
+  void Update(float dt);
+  bool CheckCollision(Rigidbody* a, Rigidbody* b);
+  //bool CheckCollisionTilemap(Rigidbody a, tilemap& tm);
 };
 
 #endif
