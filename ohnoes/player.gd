7f43c554428c5d6247bf40ef8f4ea79cb9419f33
Alpa <52045495+Alpagateau@users.noreply.github.com>
2025-04-27 02:00:38 +0200

    Modified the char controller and added jumps
    getting freaky on a friday night yeah

--- ohnoes/player.gd
+++ ohnoes/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
-
+@export var ACCEL = 10.0
+@export var MAXSPEED:float = 100.0
+@export var DRAG:float = 100
+@export var TURNACC:float = 15
+@export var JUMP_VELOCITY = 400.0
+@export var LIFT:float = 300
+var speed:float = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,14 +16,22 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		velocity.y = -JUMP_VELOCITY
+	if Input.is_action_pressed("ui_accept") and not is_on_floor():
+		velocity.y -= LIFT * delta
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		speed += direction * ACCEL * delta
+		if sign(speed) != sign(direction):
+			speed += TURNACC * delta * direction
+		if abs(speed) >= MAXSPEED:
+			speed = MAXSPEED * sign(speed)
+		velocity.x = speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		speed = move_toward(speed, 0, delta*ACCEL*DRAG)
+		velocity.x = speed
 
 	move_and_slide()
