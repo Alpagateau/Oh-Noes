fceef5f49e49b69859db4b13e1493523197754e2
Martin <alpaga.ytb@gmail.com>
2025-06-09 21:40:18 +0200

    Texture mapping for the wheel $

--- Scripts/wheel.gd
+++ Scripts/wheel.gd
@@ -5,12 +5,14 @@ extends Node2D
 @export var colors:Array[Color]
 @export var radius:float = 30
 @export var font_settings:LabelSettings
+@export var Res:Vector2i
 
 @export var min_speed:float = 1 
 @export var max_speed:float = 3
 @export var deccel:float = 2
 
 @export var mat:Material
+@export var tex:Texture2D
 
 @export_category("Debug")
 @export_tool_button("Generate Wheel") var gen = self.generate
@@ -18,35 +20,80 @@ extends Node2D
 
 var is_turnin:bool = false
 var speed:float = 0
+var dtheta:float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	generate()
 	launch()
 
+func project(pos:Vector3) -> Vector3:
+	var result:Vector3 = Vector3.ZERO
+	result.x = cos((pos.x * dtheta) - dtheta/2) * pos.y * radius
+	result.y = sin((pos.x * dtheta) - dtheta/2) * pos.y * radius
+	return result
+	
 func generate():
+	get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
 	print("Generating")
 	var children = get_children()
 	for child in children:
 		child.free()
 	var l:int = len(fxs)
-	var dtheta:float = 2*PI / l
+	dtheta= 2*PI / l
 	
 	for i in range(l):
-		var new_quarter:Polygon2D = Polygon2D.new()
-		var vertex:Array[Vector2] = [Vector2(0,0)]
-		for v in range(16):
-			vertex.append(Vector2(
-				radius *  cos( (dtheta*(v - 8))/15 ), 
-				radius *  sin( (dtheta*(v - 8))/15 )
-				))
-		vertex.append(Vector2(0, 0))
-		new_quarter.polygon = vertex
-		new_quarter.color = colors[i%len(colors)]
-		new_quarter.material = mat
-		#new_quarter.position = position
+		var mesh := ArrayMesh.new()
+		var st := SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
 		
+		var cols:int = Res.x
+		var rows:int = Res.y
+	
+		for y in range(rows):
+			for x in range(cols):
+			# Vertex positions
+				var v0 = project(Vector3(    float(x)/cols,float(y)/rows, 0))
+				var v1 = project(Vector3(  float(x+1)/cols,float(y)/rows, 0))
+				var v2 = project(Vector3(  float(x+1)/cols,float(y+1)/rows, 0))
+				var v3 = project(Vector3(    float(x)/cols ,float(y+1)/rows, 0))
+
+				# UVs: Stretch one texture across the full mesh
+				var uv0 = Vector2(float(x) / cols,     float(y) / rows)
+				var uv1 = Vector2(float(x+1) / cols, float(y) / rows)
+				var uv2 = Vector2(float(x+1) / cols, float(y+1) / rows)
+				var uv3 = Vector2(float(x) / cols,     float(y+1) / rows)
+				
+				# UVs: Stretch one texture across the full mesh
+				#var uv0 = Vector2(0,0)
+				#var uv1 = Vector2(1,0)
+				#var uv2 = Vector2(1,1)
+				#var uv3 = Vector2(0,1)
+
+			# First triangle (v0, v1, v2)
+				st.set_uv(uv0)
+				st.add_vertex(v0)
+				st.set_uv(uv1)
+				st.add_vertex(v1)
+				st.set_uv(uv2)
+				st.add_vertex(v2)
+
+			# Second triangle (v2, v3, v0)
+				st.set_uv(uv2)
+				st.add_vertex(v2)
+				st.set_uv(uv3)
+				st.add_vertex(v3)
+				st.set_uv(uv0)
+				st.add_vertex(v0)
+
+		st.generate_normals()
+		st.index()
+		st.commit(mesh)
 		
+		var mesh_instance := MeshInstance2D.new()
+		mesh_instance.mesh = mesh
+		mesh_instance.material = mat 
+		mesh_instance.texture = tex
 		var txt:Label = Label.new()
 		txt.label_settings = font_settings
 		txt.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT 
@@ -60,9 +107,9 @@ func generate():
 			txt.text = fxs[i].name
 		else:
 			txt.text = "DEBUG"
-		add_child(new_quarter)
-		new_quarter.add_child(txt)
-		new_quarter.rotation = i * dtheta
+		add_child(mesh_instance)
+		mesh_instance.add_child(txt)
+		mesh_instance.rotation = i * dtheta
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
